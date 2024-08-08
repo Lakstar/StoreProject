0e4ba36773afea8fd05d2b497d236012a3baa989
ProjectDefenseSoftuni <adsada4897@gmail.com>
2024-08-08 13:05:30 +0300

    Done with login and add

--- src/main/java/project/project/init/Init.java
+++ src/main/java/project/project/init/Init.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import project.project.model.dto.entity.*;
-import project.project.model.entity.*;
 import project.project.repository.*;
 
 import java.util.Map;
