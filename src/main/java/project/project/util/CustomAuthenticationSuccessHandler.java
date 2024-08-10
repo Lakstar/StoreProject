039d09b7ad627c057b1579520cdda7028798b64f
Lakstar <adsada4897@gmail.com>
2024-08-10 21:05:21 +0300

    Finished a working User page

---
+++ src/main/java/project/project/util/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,36 @@
+package project.project.util;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import project.project.model.entity.UserEntity;
+import project.project.repository.UserRepository;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final UserRepository userRepository;
+
+    public CustomAuthenticationSuccessHandler(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        String username = authentication.getName();
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Store userId in session
+        request.getSession().setAttribute("userId", user.getId());
+        System.out.println("User ID set in session: " + user.getId());
+
+        response.sendRedirect("/");
+    }
+}
